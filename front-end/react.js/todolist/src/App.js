56bd598a99bdc5f679bd8d49ad6e13107f11de46
HARSH062001 <89510042+HARSH062001@users.noreply.github.com>
2022-07-10 16:34:50 +0530

    commit

--- front-end/react.js/todolist/src/App.js
+++ front-end/react.js/todolist/src/App.js
@@ -1,17 +1,58 @@
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import logo from "./logo.svg";
-import { useState } from "react";
+import CounterWithNameAndSideEffect from "./CounterWithNameAndSideEffect";
+import SearchFilterDemo from "./SearchFilterDemo";
+import AddTodo from "./AddTodo";
+import TodoList from "./TodoList";
+import Welcome from "./Welcome";
 
 function App() {
   const [count, setCount] = useState(0);
-  const [anotherCounter, setAnotherCounter] = useState(0);
+  const [todos, setTodos] = useState([
+    { id: 1, text: "Wash dishes", done: false },
+    { id: 2, text: "Do laundry", done: true },
+    { id: 3, text: "Take shower", done: false },
+  ]);
 
   return (
     <div className="App">
       <img src={logo} className="App-logo" alt="logo"></img>
-      <h1>Todo List</h1>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>Click me</button>
+
+      <Routes>
+        <Route exact path="/" element={<Welcome user={"Akash"} />} />
+
+        <Route exact path="/search" element={<SearchFilterDemo />} />
+
+        <Route
+          exact
+          path="/counter"
+          element={
+            <>
+              <p>You clicked {count} times</p>
+              <button onClick={() => setCount(count + 1)}>Click me</button>
+            </>
+          }
+        />
+
+        <Route
+          exact
+          path="/counterWithNameAndSideEffect"
+          element={<CounterWithNameAndSideEffect user={"Akash Das"} />}
+        />
+
+        <Route
+          exact
+          path="/todos"
+          element={
+            <>
+              <TodoList todos={todos} setTodos={setTodos} />
+              <AddTodo setTodos={setTodos} />
+            </>
+          }
+        ></Route>
+      </Routes>
     </div>
   );
 }
